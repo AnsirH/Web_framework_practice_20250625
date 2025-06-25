c84622b3123c0f0e49ab0576146b60390f737253
AnsirH <ansir0211@gmail.com>
2025-06-25 17:48:47 +0900

    feat: index

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, detail_post
 
 urlpatterns = [
     path('', index, name="index"),
+    path('detail_post', detail_post, name="detail_post"),
 ]
